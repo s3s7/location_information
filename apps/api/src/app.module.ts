60096e75ea71b2b4f838177a6e8f2a284bef969f
shun <samesamejinbei@gmail.com>
2026-03-09 16:48:18 +0900

    refactor:不要コメントの修正

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,13 +1,3 @@
-// import { Module } from '@nestjs/common';
-// import { AppController } from './app.controller';
-// import { AppService } from './app.service';
-
-// @Module({
-//   imports: [],
-//   controllers: [AppController],
-//   providers: [AppService],
-// })
-// export class AppModule {}
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Spot } from './spots/spot.entity';
